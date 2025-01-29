a644ec2629e66b5b9cdb9c1ea4789120a1d880b6
Justin O'Connor <justinoconnor@mac.lan>
2025-01-29 12:40:41 -0500

    this appears to be working

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -2,7 +2,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
+  darkMode: ['class', '[data-theme="dark"]'],
   content: [
     "./pages/**/*.{ts,tsx}",
     "./components/**/*.{ts,tsx}",
