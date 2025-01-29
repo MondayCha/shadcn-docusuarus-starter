233b0266116641e44a8baa9727c28071a5cf41b2
Justin O'Connor <justinoconnor@mac.lan>
2025-01-29 13:28:51 -0500

    fix card border color

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -97,6 +97,9 @@ module.exports = {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out'
+      },
+      borderColor: {
+        DEFAULT: 'hsl(var(--border))'
       }
     }
   },
